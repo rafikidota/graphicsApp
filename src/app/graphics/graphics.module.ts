2d398d927bda79d167b588fc9b87d58924abb4d9
rafikidota <lesmes240698@gmail.com>
2022-08-03 21:08:27 -0400

    section 22 until video 10

--- src/app/graphics/graphics.module.ts
+++ src/app/graphics/graphics.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgChartsModule } from 'ng2-charts';
 
 import { GraphicsRoutingModule } from './graphics-routing.module';
 import { BarsComponent } from './pages/bars/bars.component';
 import { DoubleBarsComponent } from './pages/double-bars/double-bars.component';
 import { DoughnutComponent } from './pages/doughnut/doughnut.component';
 import { DoughnutHttpComponent } from './pages/doughnut-http/doughnut-http.component';
+import { BarsChartComponent } from './components/bars-chart/bars-chart.component';
 
 
 @NgModule({
@@ -13,11 +15,13 @@ import { DoughnutHttpComponent } from './pages/doughnut-http/doughnut-http.compo
     BarsComponent,
     DoubleBarsComponent,
     DoughnutComponent,
-    DoughnutHttpComponent
+    DoughnutHttpComponent,
+    BarsChartComponent
   ],
   imports: [
     CommonModule,
-    GraphicsRoutingModule
+    GraphicsRoutingModule,
+    NgChartsModule
   ]
 })
 export class GraphicsModule { }
--- src/app/graphics/pages/bars/bars.component.ts
+++ src/app/graphics/pages/bars/bars.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartEvent, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-bars',
@@ -6,6 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BarsComponent implements OnInit {
 
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+  public barChartType: ChartType = 'bar';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A',backgroundColor:'#003CFF',hoverBackgroundColor:'red' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B',backgroundColor:'#E038D2',hoverBackgroundColor:'red' },
+      { data: [33, 74, 93, 42, 26, 44, 19], label: 'Series C',backgroundColor:'#0DA3FF',hoverBackgroundColor:'red' }
+    ]
+  };
+
+  // events
+  public chartClicked({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event?: ChartEvent, active?: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public randomize(): void {
+    // Only Change 3 values
+    this.barChartData.datasets[0].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ];
+    this.barChartData.datasets[1].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ];
+    this.barChartData.datasets[2].data = [
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100)
+    ];
+
+    this.chart?.update();
+  }
   constructor() { }
 
   ngOnInit(): void {
