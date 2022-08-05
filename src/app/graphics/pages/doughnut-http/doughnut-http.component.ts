6fe414e23fae915f7a6b3d350a39fcf67d0da25c
rafikidota <lesmes240698@gmail.com>
2022-08-05 00:26:43 -0400

    section 22

--- src/app/graphics/pages/doughnut-http/doughnut-http.component.ts
+++ src/app/graphics/pages/doughnut-http/doughnut-http.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartData, ChartType, ChartConfiguration, ChartEvent } from 'chart.js';
 import { GraphicsService } from '../../services/graphics.service';
 
 @Component({
@@ -7,12 +8,59 @@ import { GraphicsService } from '../../services/graphics.service';
 })
 export class DoughnutHttpComponent implements OnInit {
 
+  // Doughnut
+  public doughnutChartLabels: string[] = [
+    //    'Download Sales', 'In-Store Sales', 'Mail-Order Sales' 
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      //   { data: [350, 450, 100], backgroundColor: ['red', 'blue', 'violet'] },
+    ]
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+  };
+
+  // events
+  public chartClicked({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({ event, active }: { event: ChartEvent, active: {}[] }): void {
+    console.log(event, active);
+  }
   constructor(private gs: GraphicsService) { }
 
   ngOnInit(): void {
-    this.gs.getSocialMediaUsers().subscribe(data=>{
-      console.log(data);
-      
+    // this.gs.getSocialMediaUsers().subscribe(data => {
+    //   const labels_array: string[] = Object.keys(data);
+    //   const data_array = Object.values(data);
+    //   const colors = ['blue', 'red', 'violet', 'pink', 'purple']
+    //   const datasets = {
+    //     data: data_array,
+    //     backgroundColor: colors
+    //   }
+    //   this.doughnutChartData.datasets.push(datasets);
+    //   for (let i = 0; i < labels_array.length; i++) {
+    //     const label = labels_array[i];
+    //     this.doughnutChartLabels.push(label);
+    //   }
+    // });
+
+    this.gs.getSocialMediaUsersDoughnut().subscribe(({ labels, values }) => {
+      const colors = ['blue', 'red', 'violet', 'pink', 'purple']
+        const datasets = {
+          data: values,
+          backgroundColor: colors
+        }
+        this.doughnutChartData.datasets.push(datasets);
+        for (let i = 0; i < labels.length; i++) {
+          const label = labels[i];
+          this.doughnutChartLabels.push(label);
+        }
     });
   }
 
--- src/app/graphics/services/graphics.service.ts
+++ src/app/graphics/services/graphics.service.ts
@@ -1,15 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraphicsService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  getSocialMediaUsers(){
+  getSocialMediaUsers() {
     return this.http.get('http://localhost:3000/graphic');
+  }
+  getSocialMediaUsersDoughnut() {
+    return this.getSocialMediaUsers().pipe(
+      delay(1500),
+      map(data => {
+        const labels = Object.keys(data);
+        const values = Object.values(data);
+        return { labels, values };
+      })
+    );
   }
 
 }
